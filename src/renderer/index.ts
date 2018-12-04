a0908f75c09db2f339015eafde4d0da31154da26
simotion <simotion@samsung-2.local>
2018-12-04 21:09:42 +0900

    made a better example of renderer code

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,3 +1,3 @@
 const styles = require('./styles/index.scss');
 
-document.write(`<div class="${styles.title} title">This is working!</div>`);
+document.write(`<div class="${styles.container}"><div class="${styles.title}">It works!</div><div class="${styles.item}">Node version: ${process.versions.node}</div><div class="${styles.item}">V8 version: ${process.versions.v8}</div><div class="${styles.item}">Erase this in /src/renderer to get started.</div></div>`);
